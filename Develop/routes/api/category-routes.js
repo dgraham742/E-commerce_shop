5376ea9c28e1e7baeb9444a9403fc1a5a610c413
dgraham742 <106363652+dgraham742@users.noreply.github.com>
2022-10-12 10:25:02 -0500

    final commit

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -6,6 +6,13 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include:[Product]
+  }).then(dataResponse => {
+    res.status(200).json(dataResponse)
+  }).catch(err => {
+    res.status(400).json(err)
+  })
 });
 
 router.get('/:id', (req, res) => {
